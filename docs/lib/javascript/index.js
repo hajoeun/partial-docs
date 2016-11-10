fa267d2066d8b666e8326ef760760cfdde99d758
Joeun <imjoeunha@gmail.com>
2016-11-10 11:55:09 +0900

    index.js: 검색 기능 오류 개선
    - 검색 기능 로직 변경 및 오류 개선

--- docs/lib/javascript/index.js
+++ docs/lib/javascript/index.js
@@ -1,10 +1,10 @@
 /* General functions */ var test;
 var make_func_list = _.T.each('d', '\
   a[href=#{{d}}]\
-    li.fn_li _.{{d}}');
+    li.fn_li[data={{d}}] _.{{d}}');
 var make_group_list = _.T.each('obj', '\
-  li#{{obj.title}}.gr_li\
-    span.gr_title {{obj.title}}\
+  li.gr_li[data={{obj.title}}]\
+    a[href=#{{obj.title}}].gr_title {{obj.title}}\
     ul.func_list\
       {{make_func_list(obj.data)}}');
 var reduce_section_data = function(data) {
@@ -27,7 +27,7 @@ var reduce_section_data = function(data) {
           }, '');
       }, '');
   }, '').replace(/\|(_+)/g, function(m, u) { return "|" + u.replace(/_/g, '&nbsp;'); }).replace(/`(.*?)`/g, '<code>$1</code>').replace(/(\n)/g, '\\$1');
-}
+};
 
 /* HTML Rendering */
 _.pipe(null,
@@ -37,7 +37,7 @@ _.pipe(null,
         p Partial JS\
     div#listbar\
       div#search\
-        input[type="text" placeholder="Search function"]\
+        input[type="text" placeholder="Search Functions" autofocus]\
       ul#group_list\
         {{make_group_list(sync)}}\
         small#no_result 검색 결과가 없습니다.\
@@ -71,7 +71,6 @@ $(document).ready(function() {
   $('pre.javascript').dblclick(function(e) {
     var cp = document.createElement('textarea');
     cp.value = e.target.innerText;
-
     e.target.appendChild(cp).select();
     document.execCommand('copy');
     cp.remove();
@@ -83,27 +82,31 @@ $(document).ready(function() {
   });
 });
 
+function hide(e) { $(e).hide(); }
+function show(e) { $(e).show(); }
+function showAll(e, childName) { $(e).find(childName).show(); $(e).show(); }
 function update_section_list(str) {
-  if (!str) return ($('#listbar li').show());
 
-  var reg = new RegExp(str, "i"), match = false;
-  var reject_group = _.reject($('ul#group_list li.gr_li'), function(group) {
-    if (group.id.match(reg)) { $('#' + group.id + ' li').show(); return match = true; }
+  str = str.replace(/\s*([?\w]+)\s*/,'$1'); // Trim
+  if (!str) return (showAll('#listbar', 'li'));
+
+  // reg: 정규표현식, G_list: 그룹 리스트, F_list: 함수명 리스트, alive: 목록 중에서 검색과 일치한 목록
+  var reg = new RegExp(str, "i"), G_list = $('#listbar li.gr_li'), F_list = $('#listbar li.fn_li'), alive = {};
+
+  _.each(F_list, function(f_list) {
+    var g_name = f_list.parentNode.parentNode.parentNode.getAttribute('data');
+    if (f_list.getAttribute('data').match(reg)) { show(f_list); alive[g_name] = true; }
+    else { hide(f_list); if (!alive.hasOwnProperty(g_name)) { alive[g_name] = false; } }
   });
 
-  _.each(reject_group, function(gr_li) {
-    var flag = false;
-
-    _.each($('#' + gr_li.id + ' ul.func_list li.fn_li'), function(li) {
-      li.innerText.match(reg) ? (flag = match) && $(li).show() : $(li).hide();
-    });
-
-    if (!flag) $(gr_li).hide();
+  _.each(G_list, function(g_list) {
+    var g_name = g_list.getAttribute('data');
+    if (g_name.match(reg)) { showAll(g_list, 'li'); }
+    else { alive[g_name] ? show(g_list) : hide(g_list); }
   });
 
-  match ? $('#no_result').hide() : $('#no_result').show();
+  _.some(alive) ? $('#no_result').hide() : $('#no_result').show();
 }
-
 
 /* Previous Code */
 // // sidebar list click event (focus animation)
